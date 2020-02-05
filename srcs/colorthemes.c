2920738f0f6e42539538bbc9a3a3f3e745f29882
Liisa Lahti <llahti@student.hive.fi>
2020-02-05 10:40:46 +0200

    Adjust color themes and move them to a separate header

--- srcs/colorthemes.c
+++ srcs/colorthemes.c
@@ -3,34 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   colorthemes.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llahti <llahti@student.hive.fi>            +#+  +:+       +#+        */
+/*   By: llahti <llahti@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/03 15:32:30 by llahti            #+#    #+#             */
-/*   Updated: 2020/02/05 09:12:52 by llahti           ###   ########.fr       */
+/*   Updated: 2020/02/05 10:39:32 by llahti           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+#include "themes.h"
 
-char	*theme_1[11] = {"10", ",0x203760", ",0x0088BF", ",0xAEDCA7",
-	",0x1A6F38", ",0x1F7518", ",0x487A15", ",0x787F12", ",0x855A0F",
-	",0xBBA681", ",0xF1F2F3"};
-
-char	*theme_2[11] = {"10", ",0xF2EE16", ",0x95ED13", ",0x36E710",
-	",0x0DE142", ",0x0BDB97", ",0x08C2D6", ",0x0667D0", ",0x040FCA",
-	",0x4901C4", ",0x9600BF"};
-
-/*char	*theme_3[7] = {"6", ",0xCCE1F2", ",0xC6F8E5", ",0xFBF7D5",
-	",0xF9DED7", ",0xF5CDDE", ",0xE2BEF1"};*/
-char	*theme_3[7] = {"6", ",0x9B01F2", ",0xD95AFE", ",0xE3DDEF",
-	",0xB896CA", ",0x151219", ",0x6F7691"};
-
-char	*theme_4[6] = {"5", ",0xFFB345", ",0xFF705E", ",0xFF406F",
-	",0x283A63", ",0x273149"};
-/*char	*theme_4[7] = {"6", ",0x0B4C3D", ",0x00755F", ",0x18925D",
-	",0x684F4B", ",0x4C3732", ",0x422E27"};*/
-
-char	**themes[THEMES] = {theme_1, theme_2, theme_3, theme_4};
 //todo tomorrow:
 //	-should the themes be put in fuctions or files so they would
 //		not be global variables?
@@ -44,7 +26,7 @@ char	**themes[THEMES] = {theme_1, theme_2, theme_3, theme_4};
 //	-try filling?
 //	-consider the rotating stuff
 
-void	ft_get_theme_colors(t_grid *grid, t_point *point)
+/*void	ft_get_theme_colors(t_grid *grid, t_point *point)
 {
 	int		steps;
 	int		color;
@@ -57,5 +39,27 @@ void	ft_get_theme_colors(t_grid *grid, t_point *point)
 	else
 		color = (steps - 1) * (point->z - grid->min_z) /
 				(grid->max_z - grid->min_z) + 1;
+	ft_get_color(point, theme[color], 0, 1);
+}*/
+
+void	ft_get_theme_colors(t_grid *grid, t_point *point)
+{
+	int		steps;
+	int		color;
+	char	**theme;
+
+	theme = themes[grid->colortheme - 1];
+	steps = ft_atoi(theme[0]);
+	if (grid->max_z - grid->min_z == 0)
+	{
+		ft_get_color(point, theme[1], 0, 1);
+		return ;
+	}
+	if (point->z == grid->min_z || (point->z < 0 && point->z < ((grid->min_z - 0) / 2)))
+		color = 1;
+	else if (point->z == grid->min_z + 2 || point->z < 0)
+		color = 2;
+	else
+		color = (steps - 3) * (point->z - 0) / (grid->max_z - 0) + 3;
 	ft_get_color(point, theme[color], 0, 1);
 }
